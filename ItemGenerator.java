0ad8bc65d62d1e3a6120b346e45325fa0e0191ca
Tyler Viacara <tyler.viacara@yahoo.com>
2019-11-21 22:24:01 +0000

    added items list in dungeon

--- ItemGenerator.java
+++ ItemGenerator.java
@@ -8,22 +8,23 @@ public class ItemGenerator{
 	private static Random randomInt = new Random();
 	public static Item generate(){
 
+
 		//creating the possible weapon item types
-		Item sword = new Item("Sword", 50, 25, 75, ItemType.Weapon);
-		Item fineSword = new Item("Fine Sword", 60, 35, 85, ItemType.Weapon);
-		Item fineSteelSword = new Item("Fine Steel Sword", 80, 55, 105, ItemType.Weapon);
+		Item sword = new Item("Sword", 50, 25, 75, (randomInt.nextInt(15) + 1, (randomInt.nextInt(17) + 1), ItemType.Weapon);
+		Item fineSword = new Item("Fine Sword", 60, 35, 85, (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Weapon);
+		Item fineSteelSword = new Item("Fine Steel Sword", 80, 55, 105, (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Weapon);
 		ArrayList<Item> weaponsList = new ArrayList<Item>(Arrays.asList(sword, fineSword, fineSteelSword));
 
 		//creating the possible armor item types
-		Item shield = new Item("Shield", 40, 30, 60, ItemType.Armor);
-		Item fineSteelShield = new Item("Fine Steel Shield", 65, 55, 85, ItemType.Armor);
-		Item bodyArmor = new Item("Body Armor", 85, 75, 105, ItemType.Armor);
+		Item shield = new Item("Shield", 40, 30, 60,  (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Armor);
+		Item fineSteelShield = new Item("Fine Steel Shield", 65, 55, 85, (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Armor);
+		Item bodyArmor = new Item("Body Armor", 85, 75, 105, (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Armor);
 		ArrayList<Item> armorList = new ArrayList<Item>(Arrays.asList(shield, fineSteelShield, bodyArmor));
 
 		//creating the possible other item types
-		Item key = new Item("Key", 10, 5, 0, ItemType.Other);
-		Item vase = new Item("Vase", 7, 3, 0, ItemType.Other);
-		Item pokeball = new Item("PokeBall", 2, 20, 0, ItemType.Other);
+		Item key = new Item("Key", 10, 5, 0,   (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Other);
+		Item vase = new Item("Vase", 7, 3, 0, (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Other);
+		Item pokeball = new Item("PokeBall", 2, 20, 0, (randomInt.nextInt(15) + 1), (randomInt.nextInt(17) + 1), ItemType.Other);
 		ArrayList<Item> otherList = new ArrayList<Item>(Arrays.asList(key, vase, pokeball));
 
 		//randomly deciding what item the user will get
